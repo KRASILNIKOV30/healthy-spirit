1b6bb8b6130ac1072319675647e9eb4efa212407
krasilnikov30 <bkrasilnikov3006@gmail.com>
2024-09-01 22:49:55 +0300

    добавил текст с обводкой

--- src/draw.py
+++ src/draw.py
@@ -4,7 +4,8 @@ from PIL import Image, ImageDraw, ImageFont
 def draw_border_on_faces(persons):
     image = Image.open("../photo.jpg")
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype("./fonts/Roboto-Regular.ttf", 36, encoding='UTF-8')
+    font_size = 40
+    font = ImageFont.truetype("./fonts/Roboto-Regular.ttf", font_size, encoding='UTF-8')
     for person in persons:
         if person["person"] == "UNDEFINED":
             color = 'red'
@@ -12,7 +13,20 @@ def draw_border_on_faces(persons):
         else:
             color = 'green'
             person_full_name = person["person"].split(' ')
-            person_name = person_full_name[0] + ' ' + person_full_name[1]
-        draw.rectangle(person["coord"], outline=color, width=3)
-        draw.text((person["coord"][0] - 5, person["coord"][3]), person_name, font=font)
+            person_name = person_full_name[1]
+        draw.rectangle(person["coord"], outline=color, width=7)
+
+        x = person["coord"][0] - 5
+        y = person["coord"][3]
+
+        fill_color = "white"
+        stroke_color = "black"
+        stroke_width = 3
+
+        draw.text((x - stroke_width, y - stroke_width), person_name, font=font, fill=stroke_color)
+        draw.text((x + stroke_width, y - stroke_width), person_name, font=font, fill=stroke_color)
+        draw.text((x - stroke_width, y + stroke_width), person_name, font=font, fill=stroke_color)
+        draw.text((x + stroke_width, y + stroke_width), person_name, font=font, fill=stroke_color)
+
+        draw.text((x, y), person_name, font=font, fill=fill_color)
     image.save("../new_photo.jpg")
