d1fee47b2578b5158fdc342d78a4b378b50470a2
Daniil Dolgorukov <daniil.dolgorukov@ispring.com>
2025-10-29 18:47:51 +0300

    добавил вытягивание даты из метаданных фотки

--- src/kb.py
+++ src/kb.py
@@ -1,11 +1,6 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 
-def make_today_button() -> ReplyKeyboardMarkup:
-    button = KeyboardButton(text="Сегодня")
-    return ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True, one_time_keyboard=True)
-
-
 def make_upload_button() -> ReplyKeyboardMarkup:
     button = KeyboardButton(text="Отметить здоровый дух")
     return ReplyKeyboardMarkup(keyboard=[[button]], resize_keyboard=True)
